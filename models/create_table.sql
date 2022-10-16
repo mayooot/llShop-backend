bc357a41b5c58f5344e4dbbc7858dd8f7903f166
Bertram Li <1129972602@qq.com>
2022-10-16 21:17:51 +0800

    update sql

--- models/create_table.sql
+++ models/create_table.sql
@@ -1,19 +1,40 @@
-CREATE TABLE `user`
-(
-    `id`          bigint                                                       NOT NULL AUTO_INCREMENT COMMENT 'ID, 范围 -2^63 ~ 2^63-1',
-    `user_id`     bigint                                                       NOT NULL COMMENT '用户ID',
-    `username`    varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci          DEFAULT 'user' COMMENT '用户名',
-    `password`    varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
-    `phone`       char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci             DEFAULT NULL COMMENT '手机号',
-    `email`       varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci          DEFAULT '' COMMENT '邮箱',
-    `avatar`      varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci         DEFAULT 'https://pic1.zhimg.com/v2-abed1a8c04700ba7d72b45195223e0ff_is.jpeg' COMMENT '头像',
-    `gender`      tinyint unsigned                                             NOT NULL DEFAULT '0' COMMENT '性别，0 -> 男, 1 -> 女, 2 -> 未知; 范围: -128~127',
-    `create_time` timestamp                                                    NULL     DEFAULT CURRENT_TIMESTAMP,
-    `update_time` timestamp                                                    NULL     DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `idx_user_id` (`user_id`) USING BTREE,
-    UNIQUE KEY `idx_phone` (`phone`) USING BTREE
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 24
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql8.0
+ Source Server Type    : MySQL
+ Source Server Version : 80019
+ Source Host           : localhost:3306
+ Source Schema         : shop
+
+ Target Server Type    : MySQL
+ Target Server Version : 80019
+ File Encoding         : 65001
+
+ Date: 16/10/2022 21:16:57
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+                         `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID, 范围 -2^63 ~ 2^63-1',
+                         `user_id` bigint NOT NULL COMMENT '用户ID',
+                         `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT 'user' COMMENT '用户名',
+                         `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '密码',
+                         `phone` char(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '手机号',
+                         `email` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '邮箱',
+                         `avatar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT 'https://pic1.zhimg.com/v2-abed1a8c04700ba7d72b45195223e0ff_is.jpeg' COMMENT '头像',
+                         `gender` tinyint UNSIGNED NOT NULL DEFAULT 0 COMMENT '性别，0 -> 男, 1 -> 女, 2 -> 未知; 范围: -128~127',
+                         `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+                         `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                         PRIMARY KEY (`id`) USING BTREE,
+                         UNIQUE INDEX `idx_user_id`(`user_id`) USING BTREE,
+                         UNIQUE INDEX `idx_phone`(`phone`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
