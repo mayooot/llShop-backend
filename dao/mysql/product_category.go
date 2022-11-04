2d3b85efd7b1ee7a64d2733e28fe1fea9fb4af97
Bertram Li <1129972602@qq.com>
2022-11-04 17:42:51 +0800

    :zap: Improve the performance of obtaining classification information

--- dao/mysql/product_category.go
+++ dao/mysql/product_category.go
@@ -15,3 +15,25 @@ func SelectAllCategory() ([]*pojo.ProductCategory, error) {
 	}
 	return categories, nil
 }
+
+// SelectFirstCategory 查询一级分类信息(level = 0)
+func SelectFirstCategory() ([]*pojo.ProductCategory, error) {
+	categories := make([]*pojo.ProductCategory, 0)
+	result := db.Where("level = 0").Find(&categories)
+	if result.Error != nil {
+		zap.L().Error("查询一级分类信息失败", zap.Error(result.Error))
+		return nil, result.Error
+	}
+	return categories, nil
+}
+
+// SelectSecondCategory 查询二级分类信息(level = 0)
+func SelectSecondCategory() ([]*pojo.ProductCategory, error) {
+	categories := make([]*pojo.ProductCategory, 0)
+	result := db.Where("level = 1").Find(&categories)
+	if result.Error != nil {
+		zap.L().Error("查询二级分类信息失败", zap.Error(result.Error))
+		return nil, result.Error
+	}
+	return categories, nil
+}
--- logic/product_category.go
+++ logic/product_category.go
@@ -4,8 +4,11 @@ import (
 	"go.uber.org/zap"
 	"shop-backend/dao/mysql"
 	"shop-backend/dao/redis"
+	"shop-backend/models/pojo"
 	"shop-backend/models/vo"
 )
+
+var errChannel = make(chan error, 2)
 
 // GetAllCategory 获取所有商品分类信息
 func GetAllCategory() ([]vo.FirstProductCategoryVO, error) {
@@ -18,43 +21,70 @@ func GetAllCategory() ([]vo.FirstProductCategoryVO, error) {
 	} // 缓存不存在，从数据库中查询，并放入缓存
 	zap.L().Info("使用商品分类信息缓存失败，查询数据库")
 
-	// 获取所有商品分类信息
-	categories, err := mysql.SelectAllCategory()
-	if err != nil {
+	// 获取商品一级分类信息
+	firstChannel := make(chan []*pojo.ProductCategory, 1)
+	// 获取商品二级分类信息
+	secondChannel := make(chan []*pojo.ProductCategory, 1)
+
+	go getFirstCategories(firstChannel)
+	go getSecondCategories(secondChannel)
+
+	firstCategories := <-firstChannel
+	secondCategories := <-secondChannel
+
+	if len(errChannel) >= 1 {
+		err := <-errChannel
+		zap.L().Error("多协程获取一级分类，二级分类出错了", zap.Error(err))
 		return nil, err
 	}
 
 	// 返回的结果切片
 	var result = make([]vo.FirstProductCategoryVO, 0)
-	for _, category := range categories {
-		// 遍历所有商品信息
-		if category.ParentID == 0 {
-			// 如果父级分类ID为0,说明为一级分类
-			firstCategory := vo.FirstProductCategoryVO{
-				ID:         category.ID,
-				Name:       category.Name,
-				Level:      category.Level,
-				ShowStatus: category.ShowStatus,
-				Icon:       category.Icon,
-			}
-			// 筛选出从属该一级分类的二级分类信息
-			for _, reCategory := range categories {
-				if reCategory.ParentID == category.ID {
-					firstCategory.SecProductCategoryList = append(firstCategory.SecProductCategoryList, &vo.SecondProductCategoryVO{
-						SecID:         reCategory.ID,
-						SecName:       reCategory.Name,
-						SecLevel:      reCategory.Level,
-						SecShowStatus: reCategory.ShowStatus,
-						SecIcon:       reCategory.Icon,
-					})
-				}
-			}
-			// 加入到返回结果切片
-			result = append(result, firstCategory)
+	for _, category := range firstCategories {
+		// 遍历所有一级分类信息
+		firstCategoryVO := vo.FirstProductCategoryVO{
+			ID:         category.ID,
+			Name:       category.Name,
+			Level:      category.Level,
+			ShowStatus: category.ShowStatus,
+			Icon:       category.Icon,
 		}
+		// 筛选出从属该一级分类的二级分类信息
+		for _, reCategory := range secondCategories {
+			if reCategory.ParentID == category.ID {
+				firstCategoryVO.SecProductCategoryList = append(firstCategoryVO.SecProductCategoryList, &vo.SecondProductCategoryVO{
+					SecID:         reCategory.ID,
+					SecName:       reCategory.Name,
+					SecLevel:      reCategory.Level,
+					SecShowStatus: reCategory.ShowStatus,
+					SecIcon:       reCategory.Icon,
+				})
+			}
+		}
+		// 加入到返回结果切片
+		result = append(result, firstCategoryVO)
 	}
-
 	// 将商品分类信息缓存进Redis
 	redis.SetCategoryList(result)
 	return result, nil
+}
+
+func getFirstCategories(channel chan []*pojo.ProductCategory) {
+	// 获取商品一级分类信息
+	firstCategories, err := mysql.SelectFirstCategory()
+	if err != nil {
+		errorChannel <- err
+	}
+	channel <- firstCategories
+	defer close(channel)
+}
+
+func getSecondCategories(channel chan []*pojo.ProductCategory) {
+	// 获取商品一级分类信息
+	secondCategories, err := mysql.SelectSecondCategory()
+	if err != nil {
+		errorChannel <- err
+	}
+	channel <- secondCategories
+	defer close(channel)
 }
